10756caea766a503241ceab1f3c14f09cf75c847
jimmy alexander castiblanco <jkastiblanco@gmail.com>
2020-05-22 00:17:13 -0500

    finalizando mediaqueries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Header from './views/header'
 
+
 function App() {
   return (
     <div>
--- src/views/header/index.js
+++ src/views/header/index.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import { mdiTwitter, mdiGithub, mdiLinkedin } from '@mdi/js'
-
+import logo from '../../assets/me.jpg'
 import Icon from '../../components/icon'
+
 
 const Header = () => {
   return (
     <section className='main'>
-      <h2 className='subtitle'>Fredy</h2>
+      <h2 className='subtitle'>Jimmy Alexander</h2>
       <h1 className='title'>
         Desarrollador frontend Javascript React y más...
       </h1>
-      <p className='description'>
-        Hola! Mi nombre es Fredy Bustos. De Colombia. Desarrolador en javascript
-        con conocimientos en librearias como React, React Native, GraphQL con
-        Apollo. Librerias como Material UI, Styled Components y un poco más. Me
-        gusta escuchar musica. Amo la vida, mi esposa y mis hijos.
-      </p>
-      <figure className='image'>
-        <img src='#' alt='profile' />
-      </figure>
+      <div className="contenido">
+          <p className='description'>
+            Hola! Mi nombre es Jimmy alexander, Colombiano emprendedor. Estudiante de Platzi actualmente con conocimientos
+            en de HTML, CSS y JS y un poco de React aunque quiero seguir aprendiendo mucho mas me gustaria ser uno de 
+            los mejores desarrolladores frontend, me gusta jugar al futbol y los videojuegos,  amo la vida que tengo y mi
+            familia.
+          </p>
+          <figure className='image'>
+            <img src={logo} alt='profile' />
+          </figure>
+      </div>
       <ul className='list'>
         <li className='list-item'>
-          <Icon icon={mdiTwitter} color='red' />
+          <Icon icon={mdiTwitter} color='cyan' />
           <a href='#' className='list-item-link'>
             Twitter
           </a>
@@ -31,11 +34,11 @@ const Header = () => {
           <a href='#'>Github</a>
         </li>
         <li>
-          <Icon icon={mdiLinkedin} />
+          <Icon icon={mdiLinkedin} color='#000099' />
           <a href='#'>Linkedin</a>
         </li>
       </ul>
-      <footer>© 2020, Built with ❤︎ by Fredy Bustos</footer>
+      <footer>© 2020, Built with ❤︎ by Jimmy Castiblanco</footer>
     </section>
   )
 }
