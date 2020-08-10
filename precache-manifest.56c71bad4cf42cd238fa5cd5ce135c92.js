591a465e820ed8dfdc1ab899facb3f519d6071ed
jimmy alexander castiblanco <jkastiblanco@gmail.com>
2020-08-10 14:40:04 -0500

    Updates

---
+++ precache-manifest.56c71bad4cf42cd238fa5cd5ce135c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0f247da95dfc49ea952b07831c2185",
+    "url": "/Mi-Portafolio/index.html"
+  },
+  {
+    "revision": "680ba23561fa0c037b10",
+    "url": "/Mi-Portafolio/static/css/main.0e6cbf2c.chunk.css"
+  },
+  {
+    "revision": "e5d8f353eaf0417964a1",
+    "url": "/Mi-Portafolio/static/js/2.10ff23c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mi-Portafolio/static/js/2.10ff23c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680ba23561fa0c037b10",
+    "url": "/Mi-Portafolio/static/js/main.6fdcd085.chunk.js"
+  },
+  {
+    "revision": "be8af0fe762021293ca4",
+    "url": "/Mi-Portafolio/static/js/runtime-main.f0c97b90.js"
+  },
+  {
+    "revision": "372eb60e00dd06b6aa32d877bef0bdb5",
+    "url": "/Mi-Portafolio/static/media/me.372eb60e.jpg"
+  }
+]);
